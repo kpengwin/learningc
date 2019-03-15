51ffb043d66148dbbec34047896bcf7027133671
Kenneth Bruce <me@kenneth-bruce.com>
2019-03-15 16:54:42 -0400

    12

--- ex12.c
+++ ex12.c
@@ -2,7 +2,26 @@
 
 int main(int argc, char*argv[]) {
 
-  
+  int areas[] = {10,12,13,14,20};
+  char name[] = "Kenny";
+  char full_name[] = {
+		      'K', 'e', 'n', 'n', 'y',
+		      ' ', 'I', '.', ' ',
+		      'B', 'r', 'u', 'c', 'e', '\0'
+  };
+
+  //may have to change %ld to %u
+  printf("The size of an int: %lu\n", sizeof(int));
+  printf("The size of areas (int[]): %lu\n", sizeof(areas));
+  printf("The number of ints in areas: %lu\n",
+	 sizeof(areas)/sizeof(int));
+  printf("The first area is %d, the second %d.\n", areas[0], areas[1]);
+  printf("The size of a char: %lu\n", sizeof(char));
+  printf("The size of name (char[]): %lu\n", sizeof(name));
+  printf("The number of chars: %lu\n", sizeof(name)/sizeof(char));
+  printf("The size of full_name (char[]): %lu\n", sizeof(full_name));
+  printf("The number of chars: %lu\n",sizeof(full_name)/sizeof(char));
+  printf("name=\"%s\" and full_name=\"%s\"\n", name, full_name);
 
   return 0;
 }
